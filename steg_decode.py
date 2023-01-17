8ce50f9301264e23f934eceb3c1c295cf6d0510c
Snir Chen <snirchen@gmail.com>
2023-01-18 00:40:34 +0200

    Renamed functions and variables

--- steg_decode.py
+++ steg_decode.py
@@ -9,7 +9,7 @@ import utils
 from config import SPACE, EMPTY, MAX_HIDE_CHANNELS, VALID_UPPERCASE_LETTERS, LEFT_PUNCTUATIONS, RIGHT_PUNCTUATIONS
 
 
-class Combination(object):
+class Text(object):
     def __init__(self, start_index: int, text: str):
         self.start_index = start_index
         self.text = text
@@ -19,9 +19,9 @@ class Combination(object):
         self.text = EMPTY
 
 
-def get_valid_words_combinations_of_channel(channel: [str]) -> [[Combination]]:
+def get_valid_symbol_combinations_of_channel(channel: [str]) -> [[Text]]:
     possible_letter_combinations = []
-    current_combination = Combination(start_index=-1, text=EMPTY)
+    current_combination = Text(start_index=-1, text=EMPTY)
 
     for index, char in enumerate(channel):
         if char in config.VALID_TEXT_SYMBOLS_LETTERS:
@@ -29,7 +29,7 @@ def get_valid_words_combinations_of_channel(channel: [str]) -> [[Combination]]:
                 if not current_combination.text == EMPTY:
                     possible_letter_combinations.append(copy.deepcopy(current_combination))
                     current_combination.clear()
-                possible_letter_combinations.append(Combination(index, SPACE))
+                possible_letter_combinations.append(Text(index, SPACE))
             else:
                 current_combination.text += char
                 if current_combination.start_index == -1:
@@ -45,89 +45,79 @@ def get_valid_words_combinations_of_channel(channel: [str]) -> [[Combination]]:
     return possible_letter_combinations
 
 
-def remove_non_valid_one_letter_words(combinations: [Combination]) -> [Combination]:
-    filtered_combinations = []
-    for combination in combinations:
-        if not len(combination.text) == 1:
-            filtered_combinations.append(combination)
+def remove_non_valid_one_letter_words(words: [Text]) -> [Text]:
+    filtered_words = []
+    for word in words:
+        if not len(word.text) == 1:
+            filtered_words.append(word)
             continue
-        elif combination.text.lower() in ['a', 'i', ' ']:
-            filtered_combinations.append(combination)
-    return filtered_combinations
+        elif word.text.lower() in ['a', 'i', ' ']:
+            filtered_words.append(word)
+    return filtered_words
 
 
-def probably_english_word(combination: Combination) -> Combination or None:
+def probably_english_word(word: Text) -> Text or None:
     """
-    This function is called when removing non-English words from the combinations.
     Notice - our MOST_COMMON_ENGLISH_WORDS list is lowercase.
-    :param combination: Combination with text which is not '' and not  ' '.
+    :param word: Combination of readable symbols with text which is not '' and not  ' '.
     :return: What is most likely to be the word in english of the given text, or None if nothing was found.
     """
-    text = combination.text
+    text = word.text
     text_lower = text.lower()
-    start_index = combination.start_index
+    start_index = word.start_index
 
     if text_lower in config.MOST_COMMON_ENGLISH_WORDS_LOWERCASE:
-        return combination
+        return word
 
     for word in config.MOST_COMMON_ENGLISH_WORDS_LOWERCASE:
         if word == text_lower:
-            return combination
+            return word
         if word in text_lower:
             if len(text_lower) - len(word) <= 1:
                 if text_lower[1:] == word:
-                    return Combination(start_index + 1, text[1:])
-                return Combination(start_index, text[:-1])
+                    return Text(start_index + 1, text[1:])
+                return Text(start_index, text[:-1])
     return None
 
 
-def remove_non_english_words(combinations: [Combination]) -> [Combination]:
-    filtered_combinations = []
+def remove_non_english_words(words: [Text]) -> [Text]:
+    filtered_words = []
 
-    for combination in combinations:
-        if combination.text == SPACE:
-            filtered_combinations.append(combination)
-        english_word_combination = probably_english_word(combination)
-        if english_word_combination:
-            filtered_combinations.append(english_word_combination)
+    for word in words:
+        if word.text == SPACE:
+            filtered_words.append(word)
 
-    return filtered_combinations
+        english_word = probably_english_word(word)
 
+        if english_word:
+            # Capital letters can be only at the start of a word
+            word_is_valid = True
+            for letter in english_word.text[1:]:
+                if letter in VALID_UPPERCASE_LETTERS:
+                    word_is_valid = False
+                    break
+            if word_is_valid:
+                filtered_words.append(english_word)
 
-def remove_wrong_capital_letters_words(combinations: [Combination]) -> [Combination]:
-    filtered_combinations = []
-    for combination in combinations:
-        if len(combination.text) == 1:
-            filtered_combinations.append(combination)
-            continue
-
-        combination_is_valid = True
-        for letter in combination.text[1:]:
-            if letter in VALID_UPPERCASE_LETTERS:
-                combination_is_valid = False
-                break
-        if combination_is_valid:
-            filtered_combinations.append(combination)
-
-    return filtered_combinations
+    return filtered_words
 
 
-def add_punctuations_if_exists(combinations: [Combination], symbols_of_channel: [str]) -> [Combination]:
-    combinations_with_punctuations = []
-    for combination in combinations:
-        previous_index = combination.start_index - 1
-        next_index = combination.start_index + len(combination.text)
+def add_punctuations_if_exists(words: [Text], symbols_of_channel: [str]) -> [Text]:
+    words_with_punctuations = []
+    for word in words:
+        previous_index = word.start_index - 1
+        next_index = word.start_index + len(word.text)
 
         # Handling (
         if previous_index >= 0:
             if symbols_of_channel[previous_index] == LEFT_PUNCTUATIONS:
-                combination.start_index = previous_index
-                combination.text = symbols_of_channel[previous_index] + combination.text
+                word.start_index = previous_index
+                word.text = symbols_of_channel[previous_index] + word.text
 
         # Handling )!?.,
         if next_index < len(symbols_of_channel):
             if symbols_of_channel[next_index] in RIGHT_PUNCTUATIONS:
-                combination.text = combination.text + symbols_of_channel[next_index]
+                word.text = word.text + symbols_of_channel[next_index]
 
         # Handling ...
         if next_index + 2 < len(symbols_of_channel):
@@ -135,72 +125,67 @@ def add_punctuations_if_exists(combinations: [Combination], symbols_of_channel: 
                                  symbols_of_channel[next_index + 1] + \
                                  symbols_of_channel[next_index + 2]
             if next_three_symbols == '...':
-                # Because we already added one dot when handled )!?., we ignore it by combination.text[:-1]
-                combination.text = combination.text[:-1] + next_three_symbols
+                # Because we already added one dot when handled )!?., we ignore it by word.text[:-1]
+                word.text = word.text[:-1] + next_three_symbols
 
-        combinations_with_punctuations.append(combination)
+        words_with_punctuations.append(word)
 
-    return combinations_with_punctuations
+    return words_with_punctuations
 
 
-def longest_strike_from_combination(i: int, all_combinations: [Combination], should_be_space=False) -> [Combination]:
-    current_combination = all_combinations[i]
+# noinspection PyTypeChecker
+def longest_strike_from_word(i: int, all_words: [Text], should_be_space=False) -> [Text]:
+    current_word = all_words[i]
 
-    if all_combinations[i].text == SPACE and not should_be_space:
+    if all_words[i].text == SPACE and not should_be_space:
         return []
-    if all_combinations[i].text != SPACE and should_be_space:
+    if all_words[i].text != SPACE and should_be_space:
         return []
 
     # Because we combined 3 hidden channels (of 3 LSBs) we may have up to 3 options to continue the strike
     possible_options_to_continue = []
-    next_start_index_should_be = all_combinations[i].start_index + len(all_combinations[i].text)
+    next_start_index_should_be = all_words[i].start_index + len(all_words[i].text)
 
-    for j in range(i + 1, len(all_combinations)):
-        if all_combinations[j].start_index < next_start_index_should_be:
+    for j in range(i + 1, len(all_words)):
+        if all_words[j].start_index < next_start_index_should_be:
             continue
-        if all_combinations[j].start_index > next_start_index_should_be:
+        if all_words[j].start_index > next_start_index_should_be:
             break
         possible_options_to_continue.append(j)
 
-    possible_strikes = [longest_strike_from_combination(j, all_combinations, not should_be_space) for j in
+    possible_strikes = [longest_strike_from_word(j, all_words, not should_be_space) for j in
                         possible_options_to_continue]
 
-    return [current_combination] if possible_strikes == [] else [current_combination] + max(possible_strikes, key=len)
+    return [current_word] if possible_strikes == [] else [current_word] + max(possible_strikes, key=len)
 
 
-def longest_words_strike(all_combinations: [Combination]) -> str:
+def longest_words_strike(all_words: [Text]) -> str:
     max_strike_len = 0
     longest_strike = []
 
-    for i in range(len(all_combinations)):
-        current_longest_strike = longest_strike_from_combination(i, all_combinations)
+    for i in range(len(all_words)):
+        current_longest_strike = longest_strike_from_word(i, all_words)
         current_strike_len = len(current_longest_strike)
         if current_strike_len > max_strike_len:
             max_strike_len = current_strike_len
             longest_strike = copy.deepcopy(current_longest_strike)
 
-    return ''.join([combination.text for combination in longest_strike])
+    return ''.join([word.text for word in longest_strike])
 
 
 def guess_hidden_text(symbols_of_channels: [[str]]) -> str:
-    valid_words_combinations_channels = [[]] * len(symbols_of_channels)
+    valid_text_channels = [[]] * len(symbols_of_channels)
 
-    for i in range(len(valid_words_combinations_channels)):
-        valid_words_combinations_channels[i] = get_valid_words_combinations_of_channel(symbols_of_channels[i])
-        valid_words_combinations_channels[i] = remove_non_valid_one_letter_words(valid_words_combinations_channels[i])
-        valid_words_combinations_channels[i] = remove_non_english_words(valid_words_combinations_channels[i])
-        valid_words_combinations_channels[i] = remove_wrong_capital_letters_words(valid_words_combinations_channels[i])
-        valid_words_combinations_channels[i] = \
-            add_punctuations_if_exists(valid_words_combinations_channels[i], symbols_of_channels[i])
+    for i in range(len(valid_text_channels)):
+        valid_text_channels[i] = get_valid_symbol_combinations_of_channel(symbols_of_channels[i])
+        valid_text_channels[i] = remove_non_valid_one_letter_words(valid_text_channels[i])
+        valid_text_channels[i] = remove_non_english_words(valid_text_channels[i])
+        valid_text_channels[i] = add_punctuations_if_exists(valid_text_channels[i], symbols_of_channels[i])
 
-    all_combinations = []
-    for channel_combination in valid_words_combinations_channels:
-        for combination in channel_combination:
-            all_combinations.append(combination)
+    all_texts = [text for valid_text_channel in valid_text_channels for text in valid_text_channel]
+    all_texts.sort(key=lambda t: t.start_index)
 
-    all_combinations.sort(key=lambda c: c.start_index)
-
-    hidden_text = longest_words_strike(all_combinations)
+    hidden_text = longest_words_strike(all_texts)
     return hidden_text
 
 
